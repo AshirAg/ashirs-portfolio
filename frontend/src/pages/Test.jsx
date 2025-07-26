33d607649b45ed8c8adf0293f25c7c8136f6e867
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-07-26 23:22:12 +0545

    Updated Files - July 26, SAT

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,10 +1,23 @@
-
+import styles from './Test.module.css'
 
 
 function Test(){
     return(
         <>
         <h3>Testing</h3>
+        <p>Hello World</p>
+
+        <div className={styles.test}>
+            <button className={styles.button_primary}>Primary Button</button>
+            <button className={styles.button_primary}>Primary Button</button>
+        </div>
+
+        <button className={`${styles.button_primary} ${styles.md}`}>Primary Button md</button>
+        <br />
+        <button className={`${styles.button_primary} ${styles.sm}`}>Primary Button sm</button>
+        <button className={`${styles.button_primary} ${styles.sm}`}><span>Hi</span><span>There</span></button>
+        <br />
+        <button className={styles.button_secondary}>Secondary Button</button>
         </>
     )
 }
