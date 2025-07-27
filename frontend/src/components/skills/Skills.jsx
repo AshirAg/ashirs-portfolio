1135d08300bf25cadf7469ccdf336fa226ff24b5
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-07-27 23:55:07 +0545

    Updated Files - July 27, SUN

--- frontend/src/components/skills/Skills.jsx
+++ frontend/src/components/skills/Skills.jsx
@@ -11,6 +11,7 @@ import cssImg from "../../assets/images/css.png"
 import jsImg from "../../assets/images/js.png"
 import reactImg from "../../assets/images/react.png"
 import phpImg from "../../assets/images/php.png"
+import bootStrapImg from "../../assets/images/bootstrap.png"
 
 function Skills() {
     const skills = [
@@ -18,71 +19,91 @@ function Skills() {
             image: pythonImg,
             title: 'Python',
             desc: 'Programming Language',
+            ratings: 3,
         },
         {
             image: javaImg,
             title: 'Java',
             desc: 'Programming Language',
+            ratings: 3,
         },
         {
             image: cImg,
             title: 'C',
             desc: 'Programming Language',
+            ratings: 3,
         },
         {
             image: figmaImg,
             title: 'Figma',
             desc: 'UI/UX Design Tool',
+            ratings: 4,
         },
         {
             image: gitImg,
             title: 'Git',
             desc: 'Version Control',
+            ratings: 2,
         },
         {
             image: mySqlImg,
             title: 'MySQL',
             desc: 'Database',
+            ratings: 4,
         },
         {
             image: sassImg,
             title: 'Sass',
             desc: 'CSS Extension',
+            ratings: 1,
         },
         {
             image: htmlImg,
             title: 'HTML5',
             desc: 'Markup language',
+            ratings: 2,
         },
         {
             image: cssImg,
             title: 'CSS',
             desc: 'Style Sheets',
+            ratings: 2,
         },
         {
             image: jsImg,
             title: 'JavaScript',
             desc: 'Scripting Language',
+            ratings: 5,
         },
         {
             image: reactImg,
             title: 'React',
             desc: 'JS FrontEnd Library',
+            ratings: 5,
         },
         {
             image: phpImg,
             title: 'PHP',
             desc: 'Fullstack/Backend',
+            ratings: 5,
+        },
+        {
+            image: bootStrapImg,
+            title: 'BootStrap',
+            desc: 'CSS Framework',
+            ratings: 1,
         }
     ];
+
+    skills.sort((a,b) => b.ratings - a.ratings);
 
     return (
         <div className="section">
             <div className="container">
                 <div className={styles.skills}>
                     {skills.map((skill, index) => (
-                        <div key={index} className={styles.card}>
-                            <div className={styles.image_card}>
+                        <div key={index} className={styles.skill_card}>
+                            <div className={styles.image_container}>
                                 <img src={skill.image} alt={skill.title} className={styles.icon} />
                             </div>
                             <h3>{skill.title}</h3>
