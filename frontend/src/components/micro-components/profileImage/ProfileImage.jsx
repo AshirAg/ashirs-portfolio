f92e5aaef4762a44c488e0c7b81beb8a939b7473
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-07-28 15:47:29 +0545

    Updated Profile Image Animation - July 28, MON

--- frontend/src/components/micro-components/profileImage/ProfileImage.jsx
+++ frontend/src/components/micro-components/profileImage/ProfileImage.jsx
@@ -1,12 +1,14 @@
 import styles from './ProfileImage.module.css'
 import profileImage from '../../../assets/images/profile-image.jpg'
+import sunGlass from '../../../assets/images/sun-glass.png'
 
 function ProfileImage(){
     return(
         <>
         <div className={styles.outer_ring}>
             <div className={styles.image_container}>
-                <img src={profileImage} alt="Profile Image" />
+                <img className={styles.profile_image} src={profileImage} alt="Profile Image" />
+                <img className={styles.emoji} src={sunGlass} alt='SunGlass' />
             </div>
         </div>
         </>
