638b06c6c9fc0f4c8f69c0dfd9e948fd7cdcbc00
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-08-06 01:09:15 +0545

    Updated Files - Aug 06, WED

--- frontend/src/components/micro-components/navbar/NavBar.jsx
+++ frontend/src/components/micro-components/navbar/NavBar.jsx
@@ -3,8 +3,8 @@ import styles from './NavBar.module.css'
 
 function NavBar(){
     return(
-        <div className={`${styles.navbar_section} section`}>
-            <div className='container'>
+        <div className={`${styles.navbar_section}`}>
+            <div className={styles.container}>
                 <div className={styles.links}>
                     <NavLink className={({ isActive }) => isActive ? styles.active : ""} to="/">Home</NavLink>
                     <NavLink className={({ isActive }) => isActive ? styles.active : ""} to="/contact">Contact</NavLink>
