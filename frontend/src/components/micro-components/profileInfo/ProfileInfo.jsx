3e83ff1f650523bc7233fa6497e9d18a3a667938
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-08-12 13:49:38 +0545

    Updated Files - Aug 12, TUE

--- frontend/src/components/micro-components/profileInfo/ProfileInfo.jsx
+++ frontend/src/components/micro-components/profileInfo/ProfileInfo.jsx
@@ -15,7 +15,7 @@ function ProfileInfo(){
             <p className={styles.desc}>Full-stack Web Developer focused on building clean & functional websites. Building websites for over 4 years.</p>
             <div className={styles.links}>
                 <Links />
-                <Button className='sm button_primary' icon={<BsFileEarmarkArrowDown />}>Resume</Button>
+                {/* <Button className='sm button_primary' icon={<BsFileEarmarkArrowDown />}>Resume</Button> */}
             </div>
         </div>
         </>
