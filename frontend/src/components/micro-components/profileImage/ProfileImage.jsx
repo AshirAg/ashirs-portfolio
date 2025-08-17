789989912aedcb0b857e064c552a9a66a6877406
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-08-17 16:57:37 +0545

    Updated Files - Aug 17, SUN

--- frontend/src/components/micro-components/profileImage/ProfileImage.jsx
+++ frontend/src/components/micro-components/profileImage/ProfileImage.jsx
@@ -1,18 +1,30 @@
+import { useEffect } from "react";
 import styles from './ProfileImage.module.css'
 import profileImage from '../../../assets/images/profile-image.webp'
 import sunGlass from '../../../assets/images/sun-glass.png'
 
 function ProfileImage(){
-    return(
-        <>
-        <div className={styles.outer_ring}>
-            <div className={styles.image_container}>
-                <img className={styles.profile_image} src={profileImage} alt="Profile Image" height={230} width={230}/>
-                <img className={styles.emoji} src={sunGlass} alt='SunGlass' width={130} />
-            </div>
-        </div>
-        </>
-    )
+  useEffect(() => {
+    const link = document.createElement("link");
+    link.rel = "preload";
+    link.as = "image";
+    link.href = profileImage; // Vite-resolved URL
+    link.type = "image/webp";
+    document.head.appendChild(link);
+
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
+
+  return(
+    <div className={styles.outer_ring}>
+      <div className={styles.image_container}>
+        <img className={styles.profile_image} src={profileImage} alt="Profile Image" height={230} width={230}/>
+        <img className={styles.emoji} src={sunGlass} alt='SunGlass' width={130} />
+      </div>
+    </div>
+  );
 }
 
 export default ProfileImage;
--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -1,23 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { Helmet } from "react-helmet";
 import Space from "../components/space/Space.jsx";
 import Header from "../components/header/Header.jsx";
 import Footer from "../components/footer/Footer.jsx";
 
-import profileImage from '../assets/images/profile-image.webp'; // WebP version
-
 function AppLayout() {
   return (
     <>
-      <Helmet>
-        <link
-          rel="preload"
-          as="image"
-          href={profileImage}
-          type="image/webp"
-        />
-      </Helmet>
-
       <Space />
       <Header />
       <Outlet />
