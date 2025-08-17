4c6ac87d3cd8484d03b7f82df58a63c7327769bd
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-08-17 13:13:35 +0545

    Updated Files - Aug 17, SUN

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"
 import Space from "../components/space/Space.jsx";
 import Header from "../components/header/Header.jsx";
 import Footer from "../components/footer/Footer.jsx";
@@ -13,7 +12,6 @@ function AppLayout(){
         <Header />
         <Outlet />
         <Footer />
-        <Analytics />
         </>
     )
 }
