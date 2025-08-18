15b5835df99936c8eabef360049dcb73bff6ae99
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-08-18 12:30:37 +0545

    Added ScrollToTop component - Aug 18, MON

--- frontend/src/layout/AppLayout.jsx
+++ frontend/src/layout/AppLayout.jsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom";
 import Space from "../components/space/Space.jsx";
 import Header from "../components/header/Header.jsx";
 import Footer from "../components/footer/Footer.jsx";
+import ScrollToTop from "../components/micro-components/ScrollToTop.jsx";
 
 function AppLayout() {
   return (
     <>
+      <ScrollToTop />
       <Space />
       <Header />
       <Outlet />
