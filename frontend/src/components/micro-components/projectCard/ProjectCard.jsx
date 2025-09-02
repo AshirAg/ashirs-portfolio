bd5bca7a46ba4ef12ebd3ea85df4a70f4dcc603b
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-09-03 00:29:48 +0545

    Added new project - Sep 03, WED

--- frontend/src/components/micro-components/projectCard/ProjectCard.jsx
+++ frontend/src/components/micro-components/projectCard/ProjectCard.jsx
@@ -4,49 +4,56 @@ import Button from "../button/Button.jsx";
 import { LuLink } from "react-icons/lu";
 import { LuGithub } from "react-icons/lu";
 
-function ProjectCard({tag_name, image, project_name, project_description, stacks, buttons = []}){
-    return(
-        <div className={styles.project_card}>
-            <div className={styles.img_section}>
-                <div className={styles.img_container}>
-                    <div className={styles.tag}>
-                        <p className={styles.tag_name}>{tag_name}</p>
-                    </div>
-                    <img src={image} alt={project_name} width={400}/>
-                </div>
-            </div>
-            <div className={styles.details}>
-                <div className={styles.info}>
-                    <h3 className={styles.project_name}>{project_name}</h3>
-                    <p className={styles.description}>{project_description}</p>
-                </div>
-                <div className={styles.stacks}>
-                    {stacks.map((item, idx) =>(
-                        <Tag key={idx}>{item}</Tag>
-                    ))}
-                </div>
-                <div className={styles.buttons}>
-                    {buttons.map((item, idx) =>(
-                        <Button 
-                            className="button_secondary sm" 
-                            icon={
-                                item === "Open Source" ? (
-                                    <LuGithub />
-                                ) : item.label === "Visit Site" ? (
-                                    <LuLink />
-                                ) : null
-                            } 
-                            key={idx}
-                            href={item.href}
-                            target={item.target}
-                        >
-                            {item.label}
-                        </Button>
-                    ))}
-                </div>
-            </div>
+function ProjectCard({
+  tag_name,
+  image,
+  project_name,
+  project_description,
+  stacks,
+  buttons = [],
+}) {
+  return (
+    <div className={styles.project_card}>
+      <div className={styles.img_section}>
+        <div className={styles.img_container}>
+          <div className={styles.tag}>
+            <p className={styles.tag_name}>{tag_name}</p>
+          </div>
+          <img src={image} alt={project_name} width={400} />
         </div>
-    )
+      </div>
+      <div className={styles.details}>
+        <div className={styles.info}>
+          <h3 className={styles.project_name}>{project_name}</h3>
+          <p className={styles.description}>{project_description}</p>
+        </div>
+        <div className={styles.stacks}>
+          {stacks.map((item, idx) => (
+            <Tag key={idx}>{item}</Tag>
+          ))}
+        </div>
+        <div className={styles.buttons}>
+          {buttons.map((item, idx) => (
+            <Button
+              className="button_secondary sm"
+              icon={
+                item.label === "Open Source" ? (
+                  <LuGithub />
+                ) : item.label === "Visit Site" ? (
+                  <LuLink />
+                ) : null
+              }
+              key={idx}
+              href={item.href}
+              target={item.target}
+            >
+              {item.label}
+            </Button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
--- frontend/src/components/projectsContainer/ProjectsContainer.jsx
+++ frontend/src/components/projectsContainer/ProjectsContainer.jsx
@@ -3,74 +3,114 @@ import styles from "./ProjectsContainer.module.css";
 import jewelryEcommProjectImage from "../../assets/images/projects/jewelry-ecomm.webp";
 import hyropowerProjectImage from "../../assets/images/projects/hydropower.webp";
 import multiVendorEcommProjectImage from "../../assets/images/projects/multi-vendor-ecomm.webp";
+import gymWebsite from "../../assets/images/projects/gym-website.webp";
 
-function ProjectsContainer({limit}){
-    const projectData = [
+function ProjectsContainer({ limit }) {
+  const projectData = [
+    {
+      tag_name: "solo",
+      image: gymWebsite,
+      project_name: "Gym website",
+      project_description:
+        "Modern and sleek designed website for gym with booking class and other general pages.",
+      stacks: ["React", "Tailwind", "Radix UI", "TypeScript"],
+      buttons: [
         {
-            tag_name: "solo",
-            image: jewelryEcommProjectImage,
-            project_name: "Jewelry eCommerce",
-            project_description: "Fully functional jewelry eCommerce website with CMS, integrated payments and an admin dashboard.",
-            stacks: [
-                "MySQL", "PHP", "JavaScript", "Figma", "CSS", "Bootstrap", 'HTML',
-            ],
-            buttons: [
-                {
-                    label: "Coming soon ...",
-                }
-            ]
+          label: "Visit Site",
+          href: "https://gym-website-topaz-eight.vercel.app/",
+          target: "_blank",
         },
+        // {
+        //   label: "Open Source",
+        //   href: "https://github.com/AshirAg/gym-website",
+        //   target: "_blank",
+        // },
+      ],
+    },
+    {
+      tag_name: "solo",
+      image: jewelryEcommProjectImage,
+      project_name: "Jewelry eCommerce",
+      project_description:
+        "Fully functional jewelry eCommerce website with CMS, integrated payments and an admin dashboard.",
+      stacks: [
+        "MySQL",
+        "PHP",
+        "JavaScript",
+        "Figma",
+        "CSS",
+        "Bootstrap",
+        "HTML",
+      ],
+      buttons: [
         {
-            tag_name: "collaborative",
-            image: hyropowerProjectImage,
-            project_name: "Hydropower Website",
-            project_description: "Dynamic web application with CMS and SEO Optimization.",
-            stacks: [
-                "MySQL", "PHP", "JavaScript", "Figma", "CSS", "Bootstrap", 'HTML',
-            ],
-            buttons: [
-                {
-                    label: "Visit Site",
-                    href: "https://www.panchkhapanhydropower.com/",
-                    target: "_blank",
-
-                }
-            ]
-
+          label: "Coming soon ...",
         },
+      ],
+    },
+    {
+      tag_name: "collaborative",
+      image: hyropowerProjectImage,
+      project_name: "Hydropower Website",
+      project_description:
+        "Dynamic web application with CMS and SEO Optimization.",
+      stacks: [
+        "MySQL",
+        "PHP",
+        "JavaScript",
+        "Figma",
+        "CSS",
+        "Bootstrap",
+        "HTML",
+      ],
+      buttons: [
         {
-            tag_name: "collaborative",
-            image: multiVendorEcommProjectImage,
-            project_name: "Multi Vendor eCommerce",
-            project_description: "Ecommerce with multiple buyers and sellers, dynamic multi role admin panel.",
-            stacks: [
-                "MySQL", "PHP", "JavaScript", "Figma", "CSS", "Bootstrap", 'HTML',
-            ],
-            buttons: [
-                {
-                    label: "Coming soon ...",
-                }
-            ]
+          label: "Visit Site",
+          href: "https://www.panchkhapanhydropower.com/",
+          target: "_blank",
         },
-    ]
+      ],
+    },
+    {
+      tag_name: "collaborative",
+      image: multiVendorEcommProjectImage,
+      project_name: "Multi Vendor eCommerce",
+      project_description:
+        "Ecommerce with multiple buyers and sellers, dynamic multi role admin panel.",
+      stacks: [
+        "MySQL",
+        "PHP",
+        "JavaScript",
+        "Figma",
+        "CSS",
+        "Bootstrap",
+        "HTML",
+      ],
+      buttons: [
+        {
+          label: "Coming soon ...",
+        },
+      ],
+    },
+  ];
 
-    const projectLimit = limit ? projectData.slice(0, limit) : projectData;
+  const projectLimit = limit ? projectData.slice(0, limit) : projectData;
 
-    return(
-        <div className={styles.project_container}>
-            {projectLimit.map((item, index) => (
-                <ProjectCard 
-                    key={index}
-                    image={item.image}
-                    project_name={item.project_name}
-                    tag_name={item.tag_name}
-                    project_description={item.project_description}
-                    stacks={item.stacks}
-                    buttons={item.buttons}
-                />
-            ))}
-        </div>
-    )
+  return (
+    <div className={styles.project_container}>
+      {projectLimit.map((item, index) => (
+        <ProjectCard
+          key={index}
+          image={item.image}
+          project_name={item.project_name}
+          tag_name={item.tag_name}
+          project_description={item.project_description}
+          stacks={item.stacks}
+          buttons={item.buttons}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ProjectsContainer;
