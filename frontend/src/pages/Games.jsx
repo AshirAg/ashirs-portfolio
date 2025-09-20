1e728ef3da647f744f5456e3c1cc842c1ded09c0
Ashir Agrawal <ashiragrawal20@gmail.com>
2025-09-20 20:06:52 +0545

    Updated Files - Sept 20, SAT

--- frontend/src/pages/Games.jsx
+++ frontend/src/pages/Games.jsx
@@ -1,64 +1,63 @@
-import styles from './css/Games.module.css';
+import styles from "./css/Games.module.css";
 import GameCard from "../components/micro-components/gameCard/GameCard";
 import tickTacToe from "../assets/games/thumbnail/tick-tac-toe.webp";
 import matchingPairs from "../assets/games/thumbnail/matching-pairs.webp";
 import comingSoon from "../assets/games/thumbnail/coming-soon.svg";
-import { Clock, Hourglass, HourglassIcon } from 'lucide-react';
+import { Clock, Hourglass, HourglassIcon } from "lucide-react";
 import { Joystick } from "lucide-react";
 
-
-
 function Games() {
+  const projectData = [
+    {
+      tag_name: "Multi Player",
+      image: tickTacToe,
+      game_name: "Tic Tac Toe",
+      href: "/game/tic-tac-toe",
+      icon: <Joystick />,
+    },
+    {
+      tag_name: "Single Player",
+      image: matchingPairs,
+      game_name: "Matching Pairs",
+      href: "/game/matching-pairs",
+      icon: <Joystick />,
+    },
+    {
+      tag_name: "Coming Soon",
+      image: comingSoon,
+      game_name: "Sudoku",
+      icon: <Hourglass />,
+    },
+  ];
 
-    const projectData = [
-        {
-            tag_name: "Multi Player",
-            image: tickTacToe,
-            game_name: "Tic Tac Toe",
-            href: "/game/tic-tac-toe",
-            icon: <Joystick />
-        },
-        {
-            tag_name: "Single Player",
-            image: matchingPairs,
-            game_name: "Matching Pairs",
-            href: "/game/matching-pairs",
-            icon: <Joystick />
-        },
-        {
-            tag_name: "Single Player",
-            image: comingSoon,
-            game_name: "Sudoku",
-            icon: <Hourglass />
-        },
-    ]
+  return (
+    <div className="section">
+      <div className={`container ${styles.container}`}>
+        <h1>Games</h1>
+        <p>
+          Welcome to the games section! Here are some games for you to kill your
+          time.{" "}
+        </p>
+        <p>
+          These games are my personal hobby projects, games will keep on adding
+          as time passes. Stay tunned for new Games.
+        </p>
 
-
-    return (
-        <div className="section">
-            <div className={`container ${styles.container}`}>
-
-                <h1>Games</h1>
-                <p>Welcome to the games section! Here are some games for you to kill your time. </p>
-                <p>These games are my personal hobby projects, games will keep on adding as time passes. Stay tunned for new Games.</p>
-
-                <div className={styles.games_container}>
-                    {projectData.map((item, index) => (
-                        <GameCard 
-                            key={index}
-                            image={item.image}
-                            game_name={item.game_name}
-                            tag_name={item.tag_name}
-                            href={item.href}
-                            icon={item.icon}
-                        />
-                    ))}
-                </div>
-
-
-            </div>
+        <div className={styles.games_container}>
+          {projectData.map((item, index) => (
+            <GameCard
+              key={index}
+              image={item.image}
+              game_name={item.game_name}
+              tag_name={item.tag_name}
+              href={item.href}
+              icon={item.icon}
+            />
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Games;
